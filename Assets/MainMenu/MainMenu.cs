c8f1eb05c35740b039aab393321f37445d5ff6d8
Cameron Fisher Haycock <Camfishhay@hotmail.com>
2023-03-23 21:19:56 +0000

    Menu Addition

---
+++ Assets/MainMenu/MainMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagment;
+
+public class MainMenu : MonoBehaviour
+{
+ 
+    public void PlayGame()
+    {
+        SceneManager.Loadscene(SceneManager.GetActiveScene().buildIn)
+    }
+
+
+}
